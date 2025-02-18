c6495f394d6d4b7df2468343230bbe81ff0d634d
Edoardo Insaghi <72695438+edoardoInsaghi@users.noreply.github.com>
2025-02-18 18:49:18 +0100

    incrocia le dita

---
+++ finetune/finetune.py
@@ -0,0 +1,115 @@
+from datasets import load_dataset
+from transformers import (
+    WhisperForConditionalGeneration,
+    WhisperProcessor,
+)
+import torch
+from torch.utils.data import DataLoader, Dataset
+from torch.optim import AdamW
+from transformers import get_scheduler
+from tqdm import tqdm
+
+
+train = load_dataset("facebook/multilingual_librispeech", "italian", split="train")
+test = load_dataset("facebook/multilingual_librispeech", "italian", split="test")
+print("Loaded data from Huggingface")
+
+model_name = "openai/whisper-tiny.en"
+processor = WhisperProcessor.from_pretrained(model_name)
+model = WhisperForConditionalGeneration.from_pretrained(model_name)
+print("Built model")
+
+
+if torch.cuda.is_available():
+    device = "cuda"
+elif torch.backends.mps.is_available():
+    device = "mps"
+else:
+    device = "cpu"
+
+print(f"Using device: {device}")
+
+class WhisperDataset(Dataset):
+    def __init__(self, dataset):
+        self.dataset = dataset
+        self.processor = processor
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        batch = self.dataset[idx]
+        return (batch["audio"]["array"], batch["transcript"])
+
+
+def collate_fn(batch):
+    input_features = torch.stack([processor.feature_extractor(item[0], sampling_rate=16000, return_tensors="pt").input_features for item in batch]).squeeze(1)
+    transcripts = [item[1] for item in batch]
+
+    _ = processor.tokenizer(transcripts, padding=True, return_tensors="pt")
+    labels = _.input_ids
+    decoder_attention_mask = _.attention_mask
+
+    return {
+        "input_features": input_features,
+        "labels": labels,
+        "decoder_attention_mask": decoder_attention_mask,
+    }
+
+
+whisper_dataset = WhisperDataset(train)
+dataloader = DataLoader(whisper_dataset, batch_size=32, shuffle=True, collate_fn=collate_fn)
+print(f"Built train dataloader, len: {len(dataloader)}")
+
+batch = next(iter(dataloader))
+print("Labels shape: ", batch["labels"].shape)
+print("Spectrograms shape: ", batch["input_features"].shape)
+print("Decoder attention mask shape: ", batch["decoder_attention_mask"].shape)
+
+model.to(device)
+model = torch.compile(model)
+torch.set_float32_matmul_precision('high')
+optimizer = AdamW(model.parameters(), lr=1e-5)
+num_epochs = 1
+num_training_steps = len(dataloader) * num_epochs
+
+lr_scheduler = get_scheduler(
+    "linear",
+    optimizer=optimizer,
+    num_warmup_steps=0,
+    num_training_steps=num_training_steps,
+)
+
+
+model.train()
+
+for epoch in range(num_epochs):
+    loop = tqdm(dataloader, leave=True)
+    total_loss = 0
+
+    for batch in loop:
+        input_features = batch["input_features"].to(device)
+        labels = batch["labels"].to(device)
+        decoder_attention_mask = batch["decoder_attention_mask"].to(device)
+
+        outputs = model(
+            input_features=input_features,
+            labels=labels,
+            decoder_attention_mask=decoder_attention_mask
+            )
+        loss = outputs.loss
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        lr_scheduler.step()
+
+        loop.set_description(f"Epoch {epoch+1}/{num_epochs}")
+        loop.set_postfix(loss=loss.item())
+
+        total_loss += loss.item()
+
+
+    print(f"Epoch {epoch+1} Loss: {total_loss / len(dataloader)}")
+
+    
--- from_scratch/config.py
+++ from_scratch/config.py
@@ -5,7 +5,8 @@ class config():
     max_duration = 200
     min_duration = 0
     audio_len = 128
-    n_text_ctx = 16
+    n_text_ctx = 4
+    encoder_hidden = 1024
     n_mels = 80
     n_audio_layer = 4
     n_text_layer = 4
@@ -13,6 +14,6 @@ class config():
     n_text_head = 6
     vocab_size = 50304
     sr = 16000
-    batch_size = 4
+    batch_size = 3
     epochs = 10000
     enc = custom_encoding()
