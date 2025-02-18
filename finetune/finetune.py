10f3ea46bccda76c2c2cecf4bff7dac197cba091
Marco Barrasso <marcobarrasso@MacBook-Air-di-Marco-2.local>
2025-02-18 21:18:29 +0100

    added wer

--- finetune/finetune.py
+++ finetune/finetune.py
@@ -4,10 +4,13 @@ from transformers import (
     WhisperProcessor,
 )
 import torch
+import os
 from torch.utils.data import DataLoader, Dataset
 from torch.optim import AdamW
 from transformers import get_scheduler
 from tqdm import tqdm
+from torch.utils.tensorboard import SummaryWriter
+from evaluation import compute_wer
 
 
 train = load_dataset("facebook/multilingual_librispeech", "italian", split="train")
@@ -50,26 +53,25 @@ def collate_fn(batch):
     labels = _.input_ids.masked_fill(_.attention_mask.ne(1), -100)
 
     return {
-        "input_features": input_features, 
+        "input_features": input_features,
         "labels": labels 
     }
 
 
-whisper_dataset = WhisperDataset(train)
-dataloader = DataLoader(whisper_dataset, batch_size=32, shuffle=True, collate_fn=collate_fn)
-print(f"Built train dataloader, len: {len(dataloader)}")
+dataset_train = WhisperDataset(train)
+dataset_test = WhisperDataset(test)
+dataloader_train = DataLoader(dataset_train, batch_size=32, shuffle=True, collate_fn=collate_fn)
+dataloader_test = DataLoader(dataset_test, batch_size=32, shuffle=True, collate_fn=collate_fn)
+print(f"Built train dataloader, len: {len(dataloader_train)}")
+print(f"Built test dataloader, len: {len(dataloader_train)}")
 
-batch = next(iter(dataloader))
-print("Labels shape: ", batch["labels"].shape)
-print("Spectrograms shape: ", batch["input_features"].shape)
-print("Decoder attention mask shape: ", batch["decoder_attention_mask"].shape)
 
 model.to(device)
 model = torch.compile(model)
 torch.set_float32_matmul_precision('high')
 optimizer = AdamW(model.parameters(), lr=1e-5)
 num_epochs = 1
-num_training_steps = len(dataloader) * num_epochs
+num_training_steps = len(dataloader_train) * num_epochs
 
 lr_scheduler = get_scheduler(
     "linear",
@@ -78,14 +80,19 @@ lr_scheduler = get_scheduler(
     num_training_steps=num_training_steps,
 )
 
+logdir = './results/tiny_no_lora'
+if not os.path.exists(logdir):
+    os.makedirs(logdir)
+writer = SummaryWriter(logdir)
+
+wer_before = compute_wer(dataloader_test, model, processor, device)
 
 model.train()
 
 for epoch in range(num_epochs):
-    loop = tqdm(dataloader, leave=True)
-    total_loss = 0
+    loop = tqdm(dataloader_train, leave=True)
 
-    for batch in loop:
+    for i, batch in enumerate(loop):
         input_features = batch["input_features"].to(device)
         labels = batch["labels"].to(device)
 
@@ -94,6 +101,7 @@ for epoch in range(num_epochs):
             labels=labels
             )
         loss = outputs.loss
+        writer.add_scalar("Loss / Train", loss, i)
 
         optimizer.zero_grad()
         loss.backward()
@@ -102,10 +110,7 @@ for epoch in range(num_epochs):
 
         loop.set_description(f"Epoch {epoch+1}/{num_epochs}")
         loop.set_postfix(loss=loss.item())
-
-        total_loss += loss.item()
-
-
-    print(f"Epoch {epoch+1} Loss: {total_loss / len(dataloader)}")
+    
+wer_after = compute_wer(dataloader_test, model, processor, device)
 
     
